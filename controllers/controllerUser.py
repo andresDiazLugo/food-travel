4e9b585aede21848f197ddd3a0cfdddcef3add8d
genjutsu <andresd.ad5@gmail.com>
2023-07-29 20:25:56 -0300

    add view login and functions

--- controllers/controllerUser.py
+++ controllers/controllerUser.py
@@ -1,10 +1,10 @@
 # from main import Main
 from models.Usuario import Usuario
 class ControllerUser:
-    def __init__(self,app):
+     def __init__(self,app):
         self.app = app
         self.session = None
-    def register_user(self):
+     def register_user(self):
             data_user = self.app.user_view.return_data_user()
             newUser = Usuario(**data_user)
             comprobate_errors = newUser.comprobate_property()
@@ -15,9 +15,28 @@ class ControllerUser:
                 if not searchUser :
                      newUser.CreateUser()
                      self.app.user_view.messageSuccess()
+                     self.navigate_login()
                 else:
-                     print("El usuario ya existe")
+                     self.app.user_view.reset_value_camp()
                      self.app.user_view.meesageError('El usuario ya esta registrado, intente iniciar sesion')
-                                   
-    def navigate_login(self):
+     def signin(self):
+          data_user_login = self.app.user_view_login.return_data_user()
+          newUserLogin = Usuario(**data_user_login)
+          comprobate_errors = newUserLogin.comprobate_property()
+          if len(comprobate_errors) > 0:
+               self.app.user_view_login.meesageError(comprobate_errors)
+          else:
+               searchUser = newUserLogin.searchUser(newUserLogin.email)
+               if searchUser:
+                    if searchUser['password'] == newUserLogin.password: 
+                         self.navigate_home()
+                    else:
+                         self.app.user_view_login.meesageError('El email o password son incorrectos, vuelve a intentar')
+               else:
+                    self.app.user_view_login.meesageError('El usuario no esta registrado en nuestro sistema,registrate')
+     def navigate_login(self):
          self.app.change_frame(self.app.user_view_login)
+     def navigate_register(self):
+         self.app.change_frame(self.app.user_view)
+     def navigate_home(self):
+          self.app.change_frame(self.app.home_view)
--- views/registerUser.py
+++ views/registerUser.py
@@ -11,48 +11,44 @@ class RegisterUser(ctk.CTkFrame):
         current_directory = os.getcwd()
         file_path = os.path.join(current_directory,'assets/img_inicio.jpg')
         # #crear otro frame
-        my_image = ctk.CTkImage(light_image=Image.open(file_path),
+        self.my_image = ctk.CTkImage(light_image=Image.open(file_path),
                                 dark_image=Image.open(file_path),
                                 size=(600,700)
                                 )
-        content_Frame = ctk.CTkFrame(self,fg_color="#03071e",width=600,height=700)
-        content_Frame.pack(side=ctk.LEFT)
-        form_content = ctk.CTkFrame(self,fg_color="#03071e",width=400,height=700)
-        form_content.pack(expand=True,fill='both')
-        label_image = ctk.CTkLabel(content_Frame,image=my_image,text='',width=600,height=700)
-        label_image.place(x=0, y=0)
-
-        # frame para el formulario
-        form_content3 = ctk.CTkFrame(form_content,fg_color="#03071e",width=700)
-        form_content3.pack(fill='both',pady=200)
+        self.content_Frame = ctk.CTkFrame(self,fg_color="#03071e",width=600,height=700)
+        self.content_Frame.pack(side=ctk.LEFT)
+        self.form_content = ctk.CTkFrame(self,fg_color="#03071e")
+        self.form_content.pack(expand=True, pady=40)
+        self.label_image = ctk.CTkLabel(self.content_Frame,image=self.my_image,text='',width=600,height=700)
+        self.label_image.place(x=0, y=0)
     
-        label_title =  ctk.CTkLabel(form_content3,text='Registrate en el sistema', text_color="white",font=ctk.CTkFont(family='Arial',size=25, weight="bold"))
-        label_title.pack()
+        self.label_title =  ctk.CTkLabel(self.form_content,text='Registrate en el sistema', text_color="white",font=ctk.CTkFont(family='Arial',size=25, weight="bold"))
+        self.label_title.pack()
         # label y inputs
-        self.nameInput = ctk.CTkEntry(form_content3,width=300,placeholder_text="ingrese nombre")
-        label_name = ctk.CTkLabel(form_content3,text='Nombre :', text_color="white")
-        label_name.pack()
+        self.nameInput = ctk.CTkEntry( self.form_content,width=300,placeholder_text="ingrese nombre")
+        self.label_name = ctk.CTkLabel( self.form_content,text='Nombre :', text_color="white")
+        self.label_name.pack()
         self.nameInput.pack()
         # 
-        self.apellidoInput = ctk.CTkEntry(form_content3,width=300,placeholder_text="ingrese apellido")
-        label_APELLIDO = ctk.CTkLabel(form_content3,text='Apellido:', text_color="white")
-        label_APELLIDO.pack()
+        self.apellidoInput = ctk.CTkEntry( self.form_content,width=300,placeholder_text="ingrese apellido")
+        self.label_APELLIDO = ctk.CTkLabel( self.form_content,text='Apellido:', text_color="white")
+        self.label_APELLIDO.pack()
         self.apellidoInput.pack()
         # 
-        self.emailInput = ctk.CTkEntry(form_content3,width=300,placeholder_text='ingrese email')
-        label_email = ctk.CTkLabel(form_content3,text='email:', text_color="white")
-        label_email.pack()
+        self.emailInput = ctk.CTkEntry( self.form_content,width=300,placeholder_text='ingrese email')
+        self.label_email = ctk.CTkLabel( self.form_content,text='email:', text_color="white")
+        self.label_email.pack()
         self.emailInput.pack()
         # 
-        self.passwordInput = ctk.CTkEntry(form_content3,width=300, placeholder_text='ingrese password')
-        label_password = ctk.CTkLabel(form_content3,text='password:', text_color="white")
-        label_password.pack()
+        self.passwordInput = ctk.CTkEntry( self.form_content,width=300, placeholder_text='ingrese password')
+        self.label_password = ctk.CTkLabel( self.form_content,text='password:', text_color="white")
+        self.label_password.pack()
         self.passwordInput.pack()
         # 
-        button_register = ctk.CTkButton(form_content3, text='Registrar',command=self.controller.register_user)
-        button_register.pack(side=ctk.LEFT,pady=10,padx=9)
-        button_login = ctk.CTkButton(form_content3, text='Iniciar Sesion',command=self.controller.navigate_login)
-        button_login.pack(side=ctk.RIGHT,pady=10,padx=9)
+        self.button_register = ctk.CTkButton( self.form_content, text='Registrar',command=self.controller.register_user)
+        self.button_register.pack(side=ctk.LEFT,pady=14,padx=9)
+        self.button_login = ctk.CTkButton( self.form_content, text='Iniciar Sesion',command=self.controller.navigate_login)
+        self.button_login.pack(side=ctk.RIGHT,pady=14,padx=9)
 
     def return_data_user(self):
             id_uniq = str(uuid.uuid4())
@@ -68,6 +64,12 @@ class RegisterUser(ctk.CTkFrame):
         messagebox.showerror('Error',info)
     def messageSuccess(self):
          messagebox.showinfo('Éxito','Tu cuenta fue registrada con exito, inicia sesion')
+    def reset_value_camp(self):
+        self.nameInput.delete(0,ctk.END)
+        self.apellidoInput.delete(0,ctk.END)
+        self.emailInput.delete(0,ctk.END)
+        self.passwordInput.delete(0,ctk.END)
+
 
 
 
