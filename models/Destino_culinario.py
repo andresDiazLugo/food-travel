8da98210a53de002832c2c719ee7836be505ec57
juanelias <juaneliascaceressoa@gmail.com>
2023-08-02 00:19:51 -0300

    modificacion de variedades de comidas en diferentes regiones

--- models/Destino_culinario.py
+++ models/Destino_culinario.py
@@ -28,3 +28,134 @@ class Destino_culinario:
             return [cls(**element) for element in data['destinos_culinarios']]
         else: 
             return False
+        
+class ComidaRegional:
+    def __init__(self, id, nombre, tipo_cocina, ingredientes, precio_minimo, precio_maximo,
+                 popularidad, disponibilidad, id_ubicacion, imagen):
+        self.id = id
+        self.nombre = nombre
+        self.tipo_cocina = tipo_cocina
+        self.ingredientes = ingredientes
+        self.precio_minimo = precio_minimo
+        self.precio_maximo = precio_maximo
+        self.popularidad = popularidad
+        self.disponibilidad = disponibilidad
+        self.id_ubicacion = id_ubicacion
+        self.imagen = imagen
+        # por que hay dos dejandolo como original para que se vea la modificacion 
+
+# Crear instancias de ComidaRegional para cada variedad de comida
+
+# Paella Valenciana
+paella_valenciana = ComidaRegional(
+    id=1,
+    nombre="Paella Valenciana",
+    tipo_cocina="Mediterránea",
+    ingredientes="Arroz, pollo, conejo, judías verdes, garrofó, pimiento rojo, tomate, azafrán, aceite de oliva",
+    precio_minimo=15,
+    precio_maximo=30,
+    popularidad="Alta",
+    disponibilidad="Todo el año",
+    id_ubicacion=1,
+    imagen="imagen_paella_valenciana.jpg"
+)
+
+# Chiles en Nogada
+chiles_en_nogada = ComidaRegional(
+    id=2,
+    nombre="Chiles en Nogada",
+    tipo_cocina="Mexicana",
+    ingredientes="Chiles poblanos, carne de cerdo y res, frutas como manzana, pera y durazno, nueces, crema y granada",
+    precio_minimo=10,
+    precio_maximo=20,
+    popularidad="Alta",
+    disponibilidad="Temporada de agosto a septiembre",
+    id_ubicacion=2,
+    imagen="imagen_chiles_en_nogada.jpg"
+)
+
+# Sushi
+sushi = ComidaRegional(
+    id=3,
+    nombre="Sushi",
+    tipo_cocina="Japonesa",
+    ingredientes="Arroz, pescado fresco, alga nori, aguacate, pepino, jengibre, wasabi",
+    precio_minimo=8,
+    precio_maximo=25,
+    popularidad="Alta",
+    disponibilidad="Todo el año",
+    id_ubicacion=3,
+    imagen="imagen_sushi.jpg"
+)
+
+# Poutine
+poutine = ComidaRegional(
+    id=4,
+    nombre="Poutine",
+    tipo_cocina="Canadiense",
+    ingredientes="Papas fritas, queso cheddar en trozos, gravy",
+    precio_minimo=5,
+    precio_maximo=12,
+    popularidad="Moderada",
+    disponibilidad="Todo el año",
+    id_ubicacion=4,
+    imagen="imagen_poutine.jpg"
+)
+
+# Rendang
+rendang = ComidaRegional(
+    id=5,
+    nombre="Rendang",
+    tipo_cocina="Indonesa",
+    ingredientes="Carne de res, leche de coco, especias, hierbas",
+    precio_minimo=10,
+    precio_maximo=18,
+    popularidad="Moderada",
+    disponibilidad="En restaurantes indonesios",
+    id_ubicacion=5,
+    imagen="imagen_rendang.jpg"
+)
+
+# Comida Italiana
+comida_italiana = ComidaRegional(
+    id=6,
+    nombre="Pizza Margherita",
+    tipo_cocina="Italiana",
+    ingredientes="Masa de pizza, tomate, mozzarella, albahaca, aceite de oliva",
+    precio_minimo=12,
+    precio_maximo=25,
+    popularidad="Alta",
+    disponibilidad="Todo el año",
+    id_ubicacion=6,
+    imagen="imagen_pizza_margherita.jpg"
+)
+
+# Comida Mediterránea
+comida_mediterranea = ComidaRegional(
+    id=7,
+    nombre="Ensalada Griega",
+    tipo_cocina="Mediterránea",
+    ingredientes="Lechuga, tomate, pepino, cebolla roja, aceitunas, queso feta, aceite de oliva, orégano",
+    precio_minimo=8,
+    precio_maximo=15,
+    popularidad="Moderada",
+    disponibilidad="Todo el año",
+    id_ubicacion=7,
+    imagen="imagen_ensalada_griega.jpg"
+)
+
+# Comida Hindú
+comida_hindu = ComidaRegional(
+    id=8,
+    nombre="Curry de Pollo",
+    tipo_cocina="Hindú",
+    ingredientes="Pollo, cebolla, tomate, jengibre, ajo, especias como cúrcuma, comino y cilantro",
+    precio_minimo=10,
+    precio_maximo=20,
+    popularidad="Moderada",
+    disponibilidad="Todo el año",
+    id_ubicacion=8
+)
+    # agregar el decorador para convertir en json o el json convertirlo en un objecto
+
+       
